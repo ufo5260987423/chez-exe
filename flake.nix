c99baa8d15352c5a61c74f99831d57954bd9f5c9
ufo5260987423 <ufo5260987423@163.com>
2024-08-04 15:12:39 +0800

    fix

--- flake.nix
+++ flake.nix
@@ -28,6 +28,9 @@
             ncurses6
           ] ++ platformSpecificInputs;
 
+          execPath = lib.makeBinPath [
+            pkgs.libtool
+          ];
           buildPhase = ''
             mkdir -p $out/{bin,lib}
             scheme --script gen-config.ss \
@@ -71,6 +74,7 @@
             # /proc 需要特殊的映射方式
             --proc /proc
             # 配置环境变量，包括查找命令和库的路径
+            --setenv PATH "${execPath}:''${PATH}"
             --setenv LD_LIBRARY_PATH "${libPath}:''${LD_LIBRARY_PATH}"
             # 映射其它根目录下的路径
             "''${auto_mounts[@]}"
